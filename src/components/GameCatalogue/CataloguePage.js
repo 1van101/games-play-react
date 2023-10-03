81f83bc701b8f3221c52b233e94c30c47c3acc8a
IvanTsvetanov <ivan.iv.cv@gmail.com>
2023-10-03 11:47:22 +0300

    games details added

--- src/components/GameCatalogue/CataloguePage.js
+++ src/components/GameCatalogue/CataloguePage.js
@@ -1,23 +1,25 @@
 import { useEffect, useState } from 'react';
+import * as gameService from '../../services/gameService';
 import GameCard from './GameCard';
 
 
-const CataloguePage = () => {
+const CataloguePage = ({
+    navigationChangeHandler
+}) => {
     const [games, setGames] = useState([])
     console.log(games)
     useEffect(() => {
-        fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-            .then(res => res.json())
+        gameService.getAllGames()
             .then(gamesResult => {
-                setGames(gamesResult)
+                setGames(gamesResult);
             })
-    }, [])
+    }, []);
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
             {games.length > 0
-                ? games.map(g => <GameCard key={g._id} game={g} />)
+                ? games.map(g => <GameCard key={g._id} game={g} navigationChangeHandler={navigationChangeHandler} />)
                 : <h3 className="no-articles">No games yet</h3>
             }
 
